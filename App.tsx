9d75cdf9b25b17fc1a2c56b2a3b0837565199bf5
Meoudev.1 <quyphat.162@gmail.com>
2024-08-28 21:50:51 +0700

    i'm rich

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable style={styles.pressable} onPress={onPressFunction}>
+        <Image
+          source={require("./Red-diamonds-scaled.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </Pressable>
     </View>
   );
 }
-
+const onPressFunction = function () {
+  alert("Hehehee");
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pressable: {
+    flex: 1,
+    width: "100%",
+    height: 300, // Đảm bảo có không gian cho hình ảnh
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "auto",
   },
 });
